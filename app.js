4c0aa44a67ec1cee43d9506a5df800dcb7359898
LucasMoraca <lucaskmoraca@gmail.com>
2026-02-26 21:41:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ let numeroScreto = 29;
 let chute = prompt('Escolha um númeor entre 1 e 100');
 
 if (numeroScreto == chute) {
-    alert('Parabéns, você acertou o número secreto!');
+    console.log('Parabéns! Você acertou o número secreto!');
 }else if (numeroScreto < chute) {
     alert('O número secreto é menor do que o número escolhido!');
 }else if (numeroScreto > chute) {
     alert('O número secreto é maior do que o número escolhido!');
 }
+
