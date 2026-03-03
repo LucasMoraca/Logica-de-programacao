4b0f5e96fc2757e1f06bfebbc482f479ab8be602
LucasMoraca <lucaskmoraca@gmail.com>
2026-03-03 16:45:37 -0300

    If, else e while
    Aprendendo a estruturação e aplicação das estruturas if e else, além da estrutura while.

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 4;
+console.log(numeroSecreto);
+let chute; // Correção: Ponto e vírgula adicionado
+let tentativas = 1; // Correção: Variável para contar o número de tentativas
 
-let chute = prompt('Escolha um númeor entre 1 e 10');
-console.log(`O número escolhido foi ${chute}`);
+// enquanto o chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
 
-let numeroScreto = 4;
-
-console.log('Resultado da comparação:', chute == numeroScreto);
-
-if (chute == numeroScreto) {
-    alert('Parabéns! Você acertou o número secreto!');
-} else {
-    console.log('O número secreto era:', numeroScreto);
-    alert('Que pena! Você errou o número secreto. Tente novamente!');
+    // se chute for igual ao numeroSecreto
+    if (chute == numeroSecreto) {
+        alert('Parabéns! Você acertou o número secreto! Número de tentativas: ' + tentativas);
+    } else {
+        if (numeroSecreto > chute) {
+            // Correção: Alterado de console.log para alert para o jogador ver a dica
+            alert(`O número secreto é maior do que o chute (${chute}).`);
+        } else {
+            // Correção: Alterado de console.log para alert para o jogador ver a dica
+            alert(`O número secreto é menor do que o chute (${chute}).`);
+        }
+        tentativas++; // Incrementa o número de tentativas
+    }
 }
-
